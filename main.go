f9832832ef21349bff25cf7c1b7ae1c41c2e9a16
Prajwal Rayal <Prajwalrayal>
2024-01-04 11:25:16 +0530

    add directory and recursive search

--- main.go
+++ main.go
@@ -4,11 +4,39 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io/fs"
+	"math"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
-func searchWordInFile(filePath, searchWord string) ([]string, error) {
+func searchPattern_dir(directory_path, searchWord string, depth int) ([]string, error) {
+	var matchingLines []string
+
+	err := filepath.WalkDir(directory_path, func(path string, dir fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if dir.IsDir() && strings.Count(path, string(os.PathSeparator)) > depth {
+			return fs.SkipDir
+		}
+
+		if !dir.IsDir() {
+			lines, err := searchPattern(path, searchWord)
+			if err != nil {
+				return err
+			}
+			matchingLines = append(matchingLines, lines...)
+		}
+		return nil
+	})
+
+	return matchingLines, err
+}
+
+func searchPattern(filePath, searchWord string) ([]string, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
@@ -33,31 +61,48 @@ func searchWordInFile(filePath, searchWord string) ([]string, error) {
 }
 
 func main() {
-	var filePath string
+	var path string
 	var searchWord string
+	var recursive bool
 
-	flag.StringVar(&filePath, "file", "", "Specify the file path")
+	flag.BoolVar(&recursive, "r", false, "Enable recursive search")
+	flag.StringVar(&path, "path", "", "Specify the file path")
 	flag.StringVar(&searchWord, "word", "", "Specify the word to search")
 
 	flag.Parse()
-
-	if filePath == "" || searchWord == "" {
+	if path == "" || searchWord == "" {
 		fmt.Println("Both file path and search word are required.")
 		return
 	}
 
-	matchingLines, err := searchWordInFile(filePath, searchWord)
+	var matchingLines []string
+	var err error
+
+	fileInfo, err := os.Stat(path)
+
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
+		panic(err)
+	}
+	depth := 0
+	if recursive {
+		depth = math.MaxInt64
+	}
+	if fileInfo.IsDir() {
+		matchingLines, err = searchPattern_dir(path, searchWord, depth)
+	} else {
+		matchingLines, err = searchPattern(path, searchWord)
+	}
+
+	if err != nil {
+		panic(err)
 	}
 
 	if len(matchingLines) > 0 {
-		fmt.Printf("The word '%s' was found in the following lines of the file '%s':\n", searchWord, filePath)
+		fmt.Printf("The word '%s' was found in the following lines :\n", searchWord)
 		for _, line := range matchingLines {
 			fmt.Println(line)
 		}
 	} else {
-		fmt.Printf("The word '%s' was not found in the file '%s'\n", searchWord, filePath)
+		fmt.Printf("The word '%s' was not found'\n", searchWord)
 	}
 }
